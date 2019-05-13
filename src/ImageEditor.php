cdbdf0e92d6038375d07cea8f9af19c6f64582f0
Alexey <info@khachatryan.org>
2019-05-14 00:13:27 +0400

    Continue of the ImageEditor class development

--- src/ImageEditor.php
+++ src/ImageEditor.php
@@ -7,14 +7,14 @@ namespace Tidepool;
 class ImageEditor
 {
     /**
-     * Brightness value: 0 - 100.
+     * Brightness value: [-100; 100].
      *
      * @var integer
     */
     private $brightness;
 
     /**
-     * Contrast value: 0 - 100.
+     * Contrast value: [-100; 100].
      *
      * @var integer
     */
@@ -74,10 +74,46 @@ class ImageEditor
 
     /**
      * Validate the request.
+     * Collect and print the validation errors.
      *
      * @return void
     */
     private function validateRequest(){
-        echo"<pre>";var_dump($this->post);die;
+        // Check if the request is post
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            echo 'The request method should be POST';
+            die;
+        }
+
+        // This is a errors array. In case if the form request is valid,
+        // This variable will be empty.
+        $errors = [];
+
+        // Check for valid filters
+        foreach ($this->post['filters'] as $filter){
+            if(!isset($this->filters[ $filter ])){
+                $errors[] = 'Wrong filter';
+                break;
+            }
+        }
+
+        // Check for valid brightness value
+        if($this->post['brightness'] < -100 || $this->post['brightness'] > 100)
+            $errors[] = 'Wrong brightness value';
+
+
+        // Check for valid brightness value
+        if($this->post['contrast'] < -100 || $this->post['contrast'] > 100)
+            $errors[] = 'Wrong contrast value';
+
+
+        // Check for valid image files
+        foreach ($this->files['images'] as $file){
+            echo"<pre>";var_dump($file);die;
+//            if(in_array($file['type'] , array(IMAGETYPE_GIF , IMAGETYPE_JPEG ,IMAGETYPE_PNG , IMAGETYPE_BMP)))
+        }
+
+
+//            echo"<pre>";var_dump($this->files);die;
     }
 }
